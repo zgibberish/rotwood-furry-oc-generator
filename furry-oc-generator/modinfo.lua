201858c2d4c542cf80a881aeccd54564f2ce225f
gibberish <gbjgms@gmail.com>
2024-09-02 21:41:03 +0700

    Modified modinfo description to match README's

--- furry-oc-generator/modinfo.lua
+++ furry-oc-generator/modinfo.lua
@@ -1,6 +1,7 @@
 name = "Furry OC Generator"
 description = [[Unlocks all character cosmetics, allows cross-species parts and colors.
-PLEASE READ: Modded characters with mismatching parts and/or colors are not compatible with vanilla Rotwood and will cause many issues if used without this mod. Please revert your modded characters to default species parts before going back to vanilla! Backing up your save files are also highly recommended!]]
+PLEASE READ: Modded characters with mismatching parts and/or colors are not compatible with vanilla Rotwood and will cause many issues if used without this mod. Please revert your modded characters to default species parts before going back to vanilla! Backing up your save files are also highly recommended!
+NOTE: The mod only forces the game to show all options, unlocked or not, and does not filter by species, it does not modify your character's progression to unlock any cosmetics.]]
 author = "gibberish"
 version = "0.1"
 api_version = 10
