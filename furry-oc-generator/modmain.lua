beecd1943d3e349c2a3c099a1b959488620db5b7
gibberish <gbjgms@gmail.com>
2024-09-06 17:29:11 +0700

    Modmain code refactoring + Removed "Remove Tail" button, replaced with empty bodypart elements, currently available for OTHER (tail) only. + Removed unneeded fmodtable require + Modified CharacterCreator.LoadFromTable to wipe all existing parts/colors every load so you can load nil parts (currently used to remove tail from CharacterScreen). + Moved SPECEIS_OVERRIDE_BUTTON_SELECTED declaration into CharacterScreen postinit because we don't need it anywhere outside anyway. + Other minor changes.

--- furry-oc-generator/modmain.lua
+++ furry-oc-generator/modmain.lua
@@ -3,8 +3,10 @@ local Cosmetic = require "defs.cosmetics.cosmetic"
 local UnlockTracker = require "components.unlocktracker"
 local CharacterScreen = require ("screens.character.characterscreen")
 local Widget = require "widgets.widget"
+local Image = require "widgets.image"
 local ScrollPanel = require "widgets.scrollpanel"
 local TextButton = require "widgets.textbutton"
+local SelectableBodyPart = require "widgets.ftf.selectablebodypart"
 local fmodtable = require "defs.sound.fmodtable"
 local UpvalueHacker = require("tools.upvaluehacker")
 local easing = require "util.easing"
@@ -12,7 +14,6 @@ local easing = require "util.easing"
 local Updater = GLOBAL.Updater
 
 local CHARACTER_SPECIES = GLOBAL.CHARACTER_SPECIES
-local REMOVE_TAIL_BUTTON_TEXT = "<p img='images/icons_emotes1/emote_mammimal_howl.tex' color=0 scale=2.5>\n\nREMOVE\nTAIL"
 
 -- update these whenever the species list is changed
 local SPECIES_OVERRIDE_BUTTON_TEXT = {
@@ -21,12 +22,14 @@ local SPECIES_OVERRIDE_BUTTON_TEXT = {
     [3] = "<p img='images/icons_emotes1/emote_amphibee_bubble_kiss.tex' color=0 scale=2.5>\n\nSPECIES\nOVERRIDE",
     [4] = "<p img='images/icons_emotes1/emote_pump.tex' color=0 scale=2.5>\n\nSPECIES\nOVERRIDE",
 }
-local SPECEIS_OVERRIDE_BUTTON_SELECTED = 1
 local SPECIES_OVERRIDE_SELECTED_TO_SPECIES = {
     [1] = nil,
     [2] = "canine",
     [3] = "mer",
     [4] = "ogre",
+}
+local EMPTY_BODYPART_AVAILABLE_OPTIONS = {
+    ["OTHER"] = true,
 }
 
 -- alow all locked parts, but this makes a dummy head part selectable
@@ -46,6 +49,24 @@ function CharacterCreator:IsBodyPartUnlocked(def, owner, ...)
 end
 function CharacterCreator:IsColorUnlocked(...)
     return true
+end
+-- make LoadFromTable wipe all bodyparts before setting parts,
+-- so nil/missing parts can be applied normally
+local original_LoadFromTable = CharacterCreator.LoadFromTable
+function CharacterCreator:LoadFromTable(data, ...)
+    print("     MODDED LoadFromTable called!")
+    print("  FROM")
+    GLOBAL.dumptable(self.bodyparts)
+    print("  TO")
+    GLOBAL.dumptable(data.bodyparts)
+    for bodypart,_ in pairs(self.bodyparts) do
+        self:SetBodyPart(bodypart, nil)
+    end
+    for colorgroup,_ in pairs(self.colorgroups) do
+        self:SetColorGroup(colorgroup, nil)
+    end
+
+    original_LoadFromTable(self, data, ...)
 end
 
 -- bypass species filtertags check in CharacterCreator:SetBodyPart
@@ -102,7 +123,6 @@ function Cosmetic.GetSpeciesColors(colorgroup, _, ...)
 
 	return total_colors
 end
-
 -- allow all other speices' body parts
 local original_GetSpeciesBodyParts = Cosmetic.GetSpeciesBodyParts
 function Cosmetic.GetSpeciesBodyParts(bodypart, _, ...)
@@ -111,6 +131,7 @@ function Cosmetic.GetSpeciesBodyParts(bodypart, _, ...)
     -- we have to go through all the species to add items to a combined list
     -- because GetSpeciesBodyParts filters by def.species, not def.filtertags
     -- which we already removed above (see Cosmetic.GetSpeciesColors)
+
     for _,species in pairs(CHARACTER_SPECIES) do
         local bodyparts = original_GetSpeciesBodyParts(bodypart, species, ...)
         for _, def in pairs(bodyparts) do
@@ -127,12 +148,12 @@ end
 -- remove the cost to change species
 -- NOTE: theres no need to reduce/remove SPECIES_CHANGE_COST since the part
 -- that charges you are already removed (see modifications to
--- GenerateBodyPartLists and OnSwitchSpecies below)
+-- GenerateBodyPartList and OnSwitchSpecies below)
 UpvalueHacker.SetUpvalue(CharacterScreen.GenerateColorList, {}, "COLOR_EXCEPTIONS")
 
 -- modifications to the character customizer screen
 AddClassPostConstruct("screens.character.characterscreen", function(self)
-    SPECEIS_OVERRIDE_BUTTON_SELECTED = 1
+    local SPECEIS_OVERRIDE_BUTTON_SELECTED = 1
 
     self.colorlist_cols = 22
 	self.colors_scroll = self.customize_character_contents:AddChild(ScrollPanel())
@@ -143,29 +164,8 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
 	 	:Offset(0, -252)
 	self.color_elements_list = self.colors_scroll:AddScrollChild(Widget("Color Options"))
 
-    self.remove_tail_button = self.panel_root:AddChild(TextButton())
-		:SetName("Remove tail button")
-		:SetTextSize(self.label_font_size)
-		:OverrideLineHeight(self.label_font_size * 0.8)
-		:SetText(REMOVE_TAIL_BUTTON_TEXT)
-		:SetTextColour(GLOBAL.UICOLORS.BACKGROUND_DARK)
-		:SetTextFocusColour(GLOBAL.UICOLORS.FOCUS_DARK)
-		:SetTextDisabledColour(GLOBAL.UICOLORS.LIGHT_TEXT_DARK)
-		:LayoutBounds("center", "center", self.panel_bg)
-		:Offset(-self.panel_w/2 + 260, -self.panel_h/2 + 180 + 220)
-		:SetOnClickFn(function() self:OnRemoveTailClicked() end)
-		:SetMultColorAlpha(1)
-		:SetShown(false)
-		:SetControlUpSound(fmodtable.Event.ui_revert_undo)
-    function self:OnRemoveTailClicked()
-        self.puppet.components.charactercreator:SetBodyPart("OTHER", nil)
-        self.owner.components.charactercreator:SetBodyPart("OTHER", nil)
-        self:ApplyChanges()
-        self:CheckForChanges()
-    end
-
     self.species_override_button = self.panel_root:AddChild(TextButton())
-        :SetName("Remove tail button")
+        :SetName("Species override button")
         :SetTextSize(self.label_font_size)
         :OverrideLineHeight(self.label_font_size * 0.8)
         :SetText(SPECIES_OVERRIDE_BUTTON_TEXT[SPECEIS_OVERRIDE_BUTTON_SELECTED])
@@ -173,7 +173,7 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
         :SetTextFocusColour(GLOBAL.UICOLORS.FOCUS_DARK)
         :SetTextDisabledColour(GLOBAL.UICOLORS.LIGHT_TEXT_DARK)
         :LayoutBounds("center", "center", self.panel_bg)
-        :Offset(-self.panel_w/2 + 260, -self.panel_h/2 + 180 + 220*2)
+        :Offset(-self.panel_w/2 + 260, -self.panel_h/2 + 180 + 220)
         :SetOnClickFn(function() self:OnSpeciesOverrideClicked() end)
         :SetMultColorAlpha(0.4)
         :SetShown(false)
@@ -193,12 +193,61 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
     local original_GenerateBodyPartList = self.GenerateBodyPartList
     function self:GenerateBodyPartList(bodypart, ...)
         original_GenerateBodyPartList(self, bodypart, ...)
+
         -- set everything, including species items to not be purchasable
         -- (this wont prevent you from using them, it just removes the corsetone
         -- tag from species items)
         for _,bodypart_element in pairs(self.scroll_contents:GetChildren()) do
             bodypart_element:SetPurchasable(false, 0)
         end
+
+        -- insert an empty option for the user to be able to remove that part
+        if EMPTY_BODYPART_AVAILABLE_OPTIONS[bodypart] then
+            local empty_bodypart_element = self.scroll_contents:AddChild(SelectableBodyPart(image_w))
+                :SetBodyPartId("")
+                :SetSelected(false)
+                :SetLocked(false)
+                :SetPurchasable(false)
+                :SetPuppetScale(0)
+                :HighlightBodyPart(nil)
+            local empty_icon = empty_bodypart_element:AddChild(Image("images/ui_ftf_dialog/convo_close.tex"))
+                :SetMultColor(GLOBAL.UICOLORS.BACKGROUND_DARK)
+                :SetMultColorAlpha(0.3)
+
+            empty_bodypart_element:SetOnClick(function()
+                self:OnBodyPartElementSelected(bodypart, {})
+                empty_bodypart_element:SetSelected(true)
+            end)
+
+            empty_bodypart_element:SetOnGainFocus(function()
+                empty_bodypart_element:OnFocusChange(true)
+                if not empty_bodypart_element:IsSelected() then
+                    self:BodyPartPreview(bodypart, nil)
+                end
+            end)
+            :SetOnLoseFocus(function()
+                empty_bodypart_element:OnFocusChange(false)
+                if not empty_bodypart_element:IsSelected() then
+                    self:ClearBodyPartPreview()
+                end
+            end)
+
+            -- refresh the scroll layout
+            local image_w = 320
+            self.scroll_contents:LayoutInDiagonal(3, 60, 60)
+                :SetPosition(-self.list_width/2 + image_w, -image_w/2)
+            self.scroll:RefreshView()
+        end
+    end
+
+    -- change nil check so preview works correctly again if you have selected the "X" empty part
+    function self:ClearBodyPartPreview()
+        -- if self.current_bodypart_group ~= nil and self.current_bodypart_name ~= nil then
+        if self.current_bodypart_group ~= nil then
+            self.puppet.components.charactercreator:SetBodyPart(self.current_bodypart_group, self.current_bodypart_name)
+        end
+        self.current_bodypart_group = nil
+        self.current_bodypart_name = nil
     end
 
     -- remove the popup when picking a species, skipping the payment too
@@ -224,7 +273,7 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
         return original_ElementTooltipFn(self, debug_mode, def, is_head, false, false, ...)
     end
 
-    -- animate the remove tail and species override buttons in like the other buttons
+    -- animate the species override button in like the other buttons
     local original__AnimateIn = self._AnimateIn
     function self:_AnimateIn()
         original__AnimateIn(self)
@@ -232,14 +281,12 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
             Updater.Series{
                 Updater.Wait(0.3),
                 Updater.Parallel{
-                    Updater.Ease(function(v) self.remove_tail_button:SetMultColorAlpha(v) end, 0, 1, 0.6, easing.outQuad),
                     Updater.Ease(function(v) self.species_override_button:SetMultColorAlpha(v) end, 0, SPECEIS_OVERRIDE_BUTTON_SELECTED == 1 and 0.4 or 1, 0.6, easing.outQuad),
                 }
             },
             Updater.Series{
                 Updater.Wait(0.5),
                 Updater.Do(function()
-                    self.remove_tail_button:Show()
                     self.species_override_button:Show()
                 end)
             }
@@ -262,8 +309,7 @@ AddClassPostConstruct("screens.character.characterscreen", function(self)
                 GLOBAL.ThePlayerData:SetCharacterCreatorSpecies(self.owner.Network:GetPlayerID(), species)
             end
         end
-        GLOBAL.TheSaveSystem:SaveCharacterForPlayerID(self.owner.Network:GetPlayerID())
-
         self.owner:PushEvent("charactercreator_load") -- to refresh species emotes
+        GLOBAL.TheSaveSystem:SaveCharacterForPlayerID(self.owner.Network:GetPlayerID())
     end
 end)
