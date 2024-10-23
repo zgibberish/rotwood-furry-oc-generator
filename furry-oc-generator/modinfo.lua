7f1c862df5b0e0c9708b042d590f457381ba7178
gibberish <gbjgms@gmail.com>
2024-10-23 20:02:25 +0700

    Updated version number to 1.0

--- furry-oc-generator/modinfo.lua
+++ furry-oc-generator/modinfo.lua
@@ -3,7 +3,7 @@ description = [[Unlocks all character cosmetics, allows cross-species parts and 
 PLEASE READ: Modded characters with mismatching parts and/or colors are not compatible with vanilla Rotwood and will cause many issues if used without this mod. Please revert your modded characters to default species parts before going back to vanilla! Backing up your save files are also highly recommended!
 NOTE: The mod only forces the game to show all options, unlocked or not, and does not filter by species, it does not modify your character's progression to unlock any cosmetics.]]
 author = "gibberish"
-version = "0.1"
+version = "1.0"
 api_version = 10
 
 dst_compatible = false
@@ -12,6 +12,7 @@ gorge_compatible = false
 dont_starve_compatible = false
 reign_of_giants_compatible = false
 shipwrecked_compatible = false
+rotwood_compatible = true
 
 client_only_mod = false
 all_clients_require_mod = true
