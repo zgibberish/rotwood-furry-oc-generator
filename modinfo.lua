e977bfebf296a062de4b509295d5244a1a5171b5
gibberish <gbjgms@gmail.com>
2025-01-18 21:04:22 +0700

    Updated modinfo version number

--- modinfo.lua
+++ modinfo.lua
@@ -3,7 +3,7 @@ description = [[Unlocks all character cosmetics, allows cross-species parts and 
 PLEASE READ: Modded characters with mismatching parts and/or colors are not compatible with vanilla Rotwood and will cause many issues if used without this mod. Please revert your modded characters to default species parts before going back to vanilla! Backing up your save files are also highly recommended!
 NOTE: The mod only forces the game to show all options, unlocked or not, and does not filter by species, it does not modify your character's progression to unlock any cosmetics.]]
 author = "gibberish"
-version = "1.0"
+version = "1.0.1"
 api_version = 10
 
 dst_compatible = false
